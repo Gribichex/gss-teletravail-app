ed327b3c2cf1af869c2e71b234e0b358925f63fe
Gribichex <petitpierre.julien@gmail.com>
2021-02-03 14:23:26 +0100

    mise a jour login

--- client/src/Login/components/LoginComponent.jsx
+++ client/src/Login/components/LoginComponent.jsx
@@ -18,48 +18,37 @@ function LoginComponent(props) {
 
   const isAvailable = () => {
     const timeout = new Promise((resolve, reject) => {
-      setTimeout(reject, 10000, "Request timed out");
+      setTimeout(reject, 20000, "Request timed out");
     });
 
     const request = fetch(url + "/api/users/status/test@test.com");
 
     return Promise.race([timeout, request]).catch(() =>
       alert(
-        "Le serveur de donnée (gratuit) est en cours de démarrage...Re-essayez ou prévenez JPP"
+        "Le serveur de donnée (gratuit) est en cours de démarrage...Re-essayez une ou deux fois puis prévenez JPP"
       )
     );
   };
 
-  trackPromise(isAvailable());
+ trackPromise(isAvailable());
 
   const LoadingSpinerComponent = (props) => {
     const { promiseInProgress } = usePromiseTracker();
     const { isValid } = useFormikContext();
+
     return (
-      <div>
+      <div style={{ display: "flex" }}>
         {promiseInProgress === true ? (
-          <div style={{ display: "flex" }}>
-            <Spinner className="mr-3" animation="border" variant="primary" />
-            <Button
-              type="submit"
-              variant="primary"
-              style={{ backgroundColor: "rgb(36,42,117)" }}
-             disabled
-            >
-              {!isRegistered ? "Inscription" : "Login"}
-            </Button>
-          </div>
-        ) :           <div style={{ display: "flex" }}>
-            
-            <Button
-              type="submit"
-              variant="primary"
-              style={{ backgroundColor: "rgb(36,42,117)" }}
-             disabled={isValid ? false : true}
-            >
-              {!isRegistered ? "Inscription" : "Login"}
-            </Button>
-          </div>}
+          <Spinner className="mr-3" animation="border" variant="primary" />
+        ) : null}
+        <Button
+          type="submit"
+          variant="primary"
+          style={{ backgroundColor: "rgb(36,42,117)" }}
+          disabled={isValid && isAvailable ? false : true}
+        >
+          {!isRegistered ? "Inscription" : "Login"}
+        </Button>
       </div>
     );
   };
