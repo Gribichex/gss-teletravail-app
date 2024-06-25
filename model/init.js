2c8dbae73a83b3510d28c5cd2ab2095e9ebb97a6
Julien PETITPIERRE <petitpierre.julien@gmail.com>
2024-06-25 15:26:17 +0200

    chore: update some dependencies to satisfy SSL requirements

--- model/init.js
+++ model/init.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@cluster0.kq2cucd.mongodb.net/"+process.env.DB_COLLECTION+"?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@cluster0.kq2cucd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex:true
