ef81672f700bc7a66407e371068ffc21c8af79fa
Gribichex <petitpierre.julien@gmail.com>
2024-06-27 16:15:30 +0200

    chore: update project with last mongoDB dependencies and practices

--- model/init.js
+++ model/init.js
@@ -1,10 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@cluster0.kq2cucd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex:true
-});
+mongoose.connect("mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@cluster0.kq2cucd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 module.exports = mongoose
--- routes/user.js
+++ routes/user.js
@@ -3,154 +3,142 @@ const authMiddleware = require("../middleware/auth");
 const router = express.Router();
 const User = require("../model/user");
 
-///////////////////////////////////Requests Targetting all Users from a specified department/////////////////////////////////////////////////////////
-router.get("/api/users",authMiddleware.requireJWT, function (req, res) {
-  let department = req.query.department;
-  let userEmail  = req.user.email;
+// Helper function to handle errors
+const handleError = (res, error) => {
+  console.error(error);
+  res.status(500).json({ error: "An internal server error occurred" });
+};
 
-  User.find({ department: department }, function (err, foundUsers) {
-    if (!err) {
-      res.send({userEmail:userEmail, foundUsers:foundUsers});
-    } else {
-      res.send(err);
-    }
-  });
+// Requests Targeting all Users from a specified department
+router.get("/api/users", authMiddleware.requireJWT, async (req, res) => {
+  try {
+    const department = req.query.department;
+    const userEmail = req.user.email;
+    const foundUsers = await User.find({ department: department });
+    res.json({ userEmail: userEmail, foundUsers: foundUsers });
+  } catch (err) {
+    handleError(res, err);
+  }
 });
 
-///!!!!!!!!!!!!!!!!!!!!A gerer pour l'authentification
-router.post("/api/users", authMiddleware.requireJWT, function (req, res) {
-  const newUser = new User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.mail,
-    department: req.body.department,
-    hash: "",
-    nonWorkingDays: [],
-  });
-
-  newUser.save(function (err) {
-    if (!err) {
-      res.send("Successfully created a new user.");
-    } else {
-      res.send(err);
-    }
-  });
-});
-
-///!!!!!!!!!!!!!!!!!!!!Dangeureux
-
-router.delete("/api/users", authMiddleware.requireJWT, function (req, res) {
-  let department = req.query.department;
-
-  User.deleteMany({ department: department }, function (err) {
-    if (!err) {
-      res.send("Successfully deleted all users.");
-    } else {
-      res.send(err);
-    }
-  });
-});
-
-////////////////////////////////Requests Targetting A Specific User/////////////////////////////
-
-router.get("/api/users/loggeduser",authMiddleware.requireJWT, function (req, res) {
-  
-  User.findOne({ email: req.user.email }, function (err, foundUser) {
-    
-    if (foundUser) {
-      res.send(foundUser);
-    } else {
-      res.send([]);
-      console.log("No user matching that e-mail was found.");
-    }
-  });
-});
-
-
-router.get("/api/users/:email",authMiddleware.requireJWT, function (req, res) {
-  
-  User.findOne({ email: req.params.email }, function (err, foundUser) {
-    
-    if (foundUser) {
-      res.send(foundUser);
-    } else {
-      res.send([]);
-      console.log("No user matching that e-mail was found.");
-    }
-  });
-});
-
-router.get("/api/users/status/:email", function (req, res) {
-  
-  User.findOne({ email: req.params.email }, function (err, foundUser) {
-    
-    if (foundUser) {
-      res.send({ userExist: true });
-    } else {
-      res.send({ userExist: false });
-      console.log("No user matching that e-mail was found.");
-    }
-  });
-});
-
-
-
-
-router.put("/api/users/:email", authMiddleware.requireJWT, function (req, res) {
-
-  User.replaceOne(
-    { mail: req.params.email },
-    {
+router.post("/api/users", authMiddleware.requireJWT, async (req, res) => {
+  try {
+    const newUser = new User({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
-      email: req.params.email,
+      email: req.body.mail,
       department: req.body.department,
-      nonWorkingDays: req.body.nonWorkingDays,
-    },
-    { overwrite: true },
-
-    function (err) {
-      if (!err) {
-        res.send(
-          "Successfully replaced the informations of the selected user."
-        );
-      } else {
-        console.log(err);
-      }
+      hash: "",
+      nonWorkingDays: [],
+    });
+    await newUser.save();
+    res.status(201).json({ message: "Successfully created a new user" });
+  } catch (err) {
+    if (err.code === 11000) { // Duplicate key error
+      res.status(409).json({ error: "User with this email already exists" });
+    } else {
+      handleError(res, err);
     }
-  );
+  }
 });
 
-router.patch(
-  "/api/users/:email",
-  authMiddleware.requireJWT,
-  function (req, res) {
-    User.update(
-      { email: req.params.email },
-      { $set: req.body },
-      function (err) {
-        if (!err) {
-          res.send("Successfully updated user.");
-        } else {
-          res.send(err);
-        }
-      }
-    );
+router.delete("/api/users", authMiddleware.requireJWT, async (req, res) => {
+  try {
+    const department = req.query.department;
+    const result = await User.deleteMany({ department: department });
+    if (result.deletedCount > 0) {
+      res.json({ message: `Successfully deleted ${result.deletedCount} users` });
+    } else {
+      res.status(404).json({ message: "No users found to delete" });
+    }
+  } catch (err) {
+    handleError(res, err);
   }
-);
+});
 
-router.delete(
-  "/api/users/:email",
-  authMiddleware.requireJWT,
-  function (req, res) {
-    User.deleteOne({ mail: req.params.email }, function (err) {
-      if (!err) {
-        res.send("Successfully deleted the corresponding user.");
-      } else {
-        res.send(err);
-      }
-    });
+// Requests Targeting A Specific User
+router.get("/api/users/loggeduser", authMiddleware.requireJWT, async (req, res) => {
+  try {
+    const foundUser = await User.findOne({ email: req.user.email });
+    if (foundUser) {
+      res.json(foundUser);
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (err) {
+    handleError(res, err);
   }
-);
+});
+
+router.get("/api/users/:email", authMiddleware.requireJWT, async (req, res) => {
+  try {
+    const foundUser = await User.findOne({ email: req.params.email });
+    if (foundUser) {
+      res.json(foundUser);
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (err) {
+    handleError(res, err);
+  }
+});
+
+router.get("/api/users/status/:email", async (req, res) => {
+  try {
+    const foundUser = await User.findOne({ email: req.params.email });
+    res.json({ userExist: !!foundUser });
+  } catch (err) {
+    handleError(res, err);
+  }
+});
+
+router.put("/api/users/:email", authMiddleware.requireJWT, async (req, res) => {
+  try {
+    const result = await User.replaceOne(
+      { email: req.params.email },
+      {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.params.email,
+        department: req.body.department,
+        nonWorkingDays: req.body.nonWorkingDays,
+      },
+      { overwrite: true }
+    );
+    if (result.modifiedCount > 0) {
+      res.json({ message: "Successfully updated user information" });
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (err) {
+    handleError(res, err);
+  }
+});
+
+router.patch("/api/users/:email", authMiddleware.requireJWT, async (req, res) => {
+  try {
+    const result = await User.updateOne({ email: req.params.email }, { $set: req.body });
+    if (result.modifiedCount > 0) {
+      res.json({ message: "Successfully updated user" });
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (err) {
+    handleError(res, err);
+  }
+});
+
+router.delete("/api/users/:email", authMiddleware.requireJWT, async (req, res) => {
+  try {
+    const result = await User.deleteOne({ email: req.params.email });
+    if (result.deletedCount > 0) {
+      res.json({ message: "Successfully deleted the user" });
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (err) {
+    handleError(res, err);
+  }
+});
 
 module.exports = router;
