87fe9a880e28bb48c2365991f1d908496ea4940a
Gribichex <petitpierre.julien@gmail.com>
2024-06-28 11:31:38 +0200

    fix: fix missing dbURI

--- model/init.js
+++ model/init.js
@@ -17,9 +17,13 @@ switch(env) {
   default:
     dbName = 'dev_db';
 }
+
+// Construct the full MongoDB URI
+const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kq2cucd.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
+
 // Connect to MongoDB
 mongoose.connect(dbURI)
-  .then(() => console.log(`Connected to MongoDB (${process.env.NODE_ENV} environment)`))
+  .then(() => console.log(`Connected to MongoDB (${env} environment)`))
   .catch((err) => console.error('Could not connect to MongoDB', err));
 
 module.exports = mongoose;
